f4297709a20ad8d9f28ca55d00b7b6217fc867a1
Max Chou <max9159@gmail.com>
2022-07-18 23:42:37 +0800

    Generate mock data on store service

--- service/storeService.js
+++ service/storeService.js
@@ -1,6 +1,10 @@
 class StoreService {
   _data = {};
   constructor() {
+    // Generate mock data to pretend data from a DataBase
+    for (let i = 0; i < 10000; i++) {
+      this._data[i] = `mock_data_${i}`
+    }
   }
 
   get(key) {
